331f90ba882b366ce653b204df93d721438ada03
Marc Katz <marc.f.katz@gmail.com>
2023-08-14 16:50:29 -0400

    create backbone table and ColumnCard components

--- client/src/components/ColumnCard.js
+++ client/src/components/ColumnCard.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function ColumnCard({column}) {
+    const {id, name, column_type_string, is_pk} = column
 
+    useEffect(()=>{
+        console.log('ok')
+    },[])
 
     return (
-        <div>
-            Column
+        <div className="row">
+            <div className="col">Column</div>
+            <div className="col">{name}</div>
+            <div className="col">{column_type_string}</div>
         </div>
     )
 }
--- client/src/components/TableCard.js
+++ client/src/components/TableCard.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ColumnCard from "./ColumnCard";
 
 function TableCard({tableId}) {
+    const [columns, setColumns] = useState([])
+    const [tableName, setName] = useState('')
 
     useEffect(() => {
+        //TODO: Fix proxy
         fetch(`http://localhost:5555/tables/${tableId}`)
-        .then(r=>r.json())
-        .then(table => console.log(table))
+        .then(r => r.json())
+        .then(t => {
+            setColumns(t.columns)
+            setName(t.name)
+        })
+        // .then(t => setTable(t))
     },[])
 
+    const create_rows = columns.map(col => {
+            return <ColumnCard key={col.id} column={col}/>
+        });
+
     return (
-        <div>
-            Table
+        <div className="container">
+            <div className="row">{tableName}</div>
+            {create_rows}
         </div>
     )
 }
