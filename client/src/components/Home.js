f96707ffbafbb7932c59ffdb1b5ec93ca10c0151
Marc Katz <marc.f.katz@gmail.com>
2023-08-17 13:59:29 -0400

    Add logo and custom icons, edit html metadata

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/user";
+import logo from "../erlogo.png"
 
 function Home(){
     const {user} = useContext(UserContext)
 
     return (
         <div>
+            <img src={logo} alt='Erlenmeyer' />
             {user ? (
                 'Logged In'
             ) : (
