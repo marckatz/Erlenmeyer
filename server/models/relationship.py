76c178a7839ffc0b8846c76884a7e662cb29f935
Marc Katz <marc.f.katz@gmail.com>
2023-08-23 14:23:17 -0400

    add constraint to relationship table

--- server/models/relationship.py
+++ server/models/relationship.py
@@ -8,6 +8,8 @@ class Relationship(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     from_many_id = db.Column(db.Integer, db.ForeignKey('columns.id'))
     to_one_id = db.Column(db.Integer, db.ForeignKey('columns.id'))
+    
+    __table_args__ = ( db.UniqueConstraint(from_many_id, from_many_id), )
 
     from_many = db.relationship('Column', foreign_keys=[from_many_id], back_populates='from_many_relationships')
     to_one = db.relationship('Column', foreign_keys=[to_one_id], back_populates='to_one_relationships')
