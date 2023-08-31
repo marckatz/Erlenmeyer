90a1076ae33240a6cd42928096a13f3926490ee7
Marc Katz <marc.f.katz@gmail.com>
2023-08-30 23:08:11 -0400

    replace python file with flask run in launch

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pipenv install
 npm install --prefix client
-pipenv run python server/app.py & npm start --prefix client
+export FLASK_APP=server/app.py
+export FLASK_RUN_PORT=5555
+pipenv run flask run & npm start --prefix client
 exit 0
